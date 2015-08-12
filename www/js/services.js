6fda77094d561e0186be472713773ae83ed658ea
medhoover <gmedhoover@gmail.com>
2015-08-12 12:10:30 +0100

    Updated service.js to be compatible with local enviroment

--- www/js/services.js
+++ www/js/services.js
@@ -1,14 +1,14 @@
 angular.module('starter.services', [])
 
 .factory('Recette', function($resource){
-      return $resource('http://bonprixtunisie.com/celialib/web/recettes/:id ',{id: '@id'}, {
+      return $resource('http://localhost/celialib/web/recettes/:id ',{id: '@id'}, {
                           'query': { method:'GET', isArray:true, responseType:'json' },
                            'get': { method:'GET', isArray:false, responseType:'json' }
                         });
     })
 
 .factory('Produit', function($resource){
-      return $resource('http://bonprixtunisie.com/celialib/web/produits/ ',{}, {
+      return $resource('http://localhost/celialib/web/produits/ ',{}, {
                           'query': { method:'GET', isArray:true}
         });
     });
